3f470b0be2eee15f0ad99c257c636831d599976a
Seth Johnson <seth@rayjseth.io>
2026-03-15 11:02:33 -0700

    fix: remove ldflags from makefile, get version from buildinfo (#6)

--- internal/cli/flags.go
+++ internal/cli/flags.go
@@ -1,6 +1,14 @@
 package cli
 
+import "runtime/debug"
+
 var Version = "dev"
+
+func init() {
+	if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" && info.Main.Version != "(devel)" {
+		Version = info.Main.Version
+	}
+}
 
 type Flags struct {
 	Min     float64
